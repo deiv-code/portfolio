2c800271f586e63c04a6cf0b6102ddc31e318982
David Dume <dumedavid11@gmail.com>
2023-11-02 16:56:43 +0100

    test

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="my-20">
-        <div class="bg-[#00A38C] rounded-xl mx-10 lg:mx-72 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
+        <div class="bg-[#00A38C] rounded-xl mx-2 md:mx-10 lg:mx-72 shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)]">
             <div class="text-center py-20">
                 <h4 class="text-[#CFF5FF] uppercase text-xl font-bold tracking-widest">contact me</h4>
                 <h1 class="pt-10 text-3xl">
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -8,7 +8,7 @@
         <div class="xl:mx-20 mx-10" v-for="project in projects">
             <div class="lg:flex items-center sm:text-center mb-10">
                 <div class="mx-auto">
-                    <img :src="project.image" class="max-w-[30rem] mx-auto" alt="">
+                    <img :src="project.image" class="max-w-[30rem]" alt="">
                 </div>
                 <div class="lg:pl-20">
                     <h4 class="font-bold tracking-wider text-2xl my-10">{{ project.title }}</h4>
@@ -28,13 +28,13 @@
 
     const projects = ref([
         {
-            image: 'src/assets/fantachatpc.png',
+            image: '../assets/fantachatpc.png',
             title: 'Fanta Chat',
             desc: "Designed for 'FantaCalcio' fans, this web app gives Serie A player insights and advice to the users, thanks to ChatGPT’s API. It's crafted with Vue.js, Nuxt.js for backend integration, styled with Tailwind CSS, and powered by the OpenAI API for chat and embeddings. During this project, I've honed my skills in Nuxt.js, dabbled in web scraping, and started navigating the world of vector databases, broadening my development toolkit.",
             link: 'https://fantachat-daviddume.vercel.app/'
         },
         {
-            image: 'src/assets/moviespc.png',
+            image: '../assets/moviespc.png',
             title: 'Nuxt Movies',
             desc: 'This is the very first app i made using NuxtJS. Its a simple webapp that lets the users search for their favourite movies using MovieDB APIs. While building this app i learned how to build a functional website with NuxtJS and how to use routes.',
             link: 'https://nuxt-movies-eta.vercel.app/'
