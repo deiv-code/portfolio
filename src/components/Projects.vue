4a65e0242d8f2d4491846ebb1be44008e2bf22a1
deiv-hash <fantachat88@gmail.com>
2025-03-09 12:54:26 +0200

    Update Projects section with new project images and descriptions

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,47 +1,103 @@
 <template>
-    <section class="xl:mx-[10%]">
-        <div class="text-center py-20">
-            <h2 class="uppercase text-3xl font-bold tracking-wider">projects</h2>
-            <div class="w-[20px] mx-auto bg-emerald-500 h-1 rounded-lg my-6" />
-            <p class="mx-20 text-lg">Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
+  <section class="xl:mx-[10%]">
+    <div class="text-center py-20">
+      <h2 class="uppercase text-3xl font-bold tracking-wider">projects</h2>
+      <div class="w-[20px] mx-auto bg-emerald-500 h-1 rounded-lg my-6" />
+      <p class="mx-20 text-lg">
+        Here you will find more information about me, what I do, and my current
+        skills mostly in terms of programming and technology
+      </p>
+    </div>
+
+    <div
+      class="lg:flex justify-between items-center md:text-center mb-10 xl:mx-20 mx-10"
+    >
+      <div class="">
+        <img src="../assets/fantachat.png" class="mx-auto" alt="" />
+      </div>
+      <div class="lg:px-20">
+        <h4 class="font-bold tracking-wider text-2xl my-10">Fanta Chat</h4>
+        <p class="leading-8 text-gray-500 text-lg">
+          Designed for 'FantaCalcio' fans, this web app gives Serie A player
+          insights and advice to the users, thanks to ChatGPT's API. It's
+          crafted with Next.js for frontend and backend integration, styled with
+          Tailwind CSS, and powered by the OpenAI API for chat and embeddings.
+          During this project, I've honed my skills in Next.js, dabbled in web
+          scraping, and started navigating the world of vector databases,
+          broadening my development toolkit.
+        </p>
+        <div
+          class="w-fit my-10 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] mx-auto"
+        >
+          <a
+            href="https://www.fanta.chat/"
+            class="uppercase py-3 px-14 text-lg text-white font-bold bg-emerald-500 rounded-md"
+            >visit</a
+          >
         </div>
+      </div>
+    </div>
 
-            <div class="lg:flex justify-between items-center md:text-center mb-10 xl:mx-20 mx-10">
-                <div class="">
-                    <img src="../assets/fantachatpc.png" class="mx-auto" alt="">
-                </div>
-                <div class="lg:px-20">
-                    <h4 class="font-bold tracking-wider text-2xl my-10">Fanta Chat</h4>
-                    <p class="leading-8 text-gray-500 text-lg">Designed for 'FantaCalcio' fans, this web app gives Serie A player insights and advice to the users, thanks to ChatGPT’s API. It's crafted with Vue.js, Nuxt.js for backend integration, styled with Tailwind CSS, and powered by the OpenAI API for chat and embeddings. During this project, I've honed my skills in Nuxt.js, dabbled in web scraping, and started navigating the world of vector databases, broadening my development toolkit.</p>
-                    <div class="w-fit my-10 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] mx-auto">
-                        <a href="https://fantachat-daviddume.vercel.app/" class="uppercase py-3 px-14 text-lg text-white font-bold bg-emerald-500 rounded-md">visit</a>
-                    </div>
-                </div>
+    <div
+      class="lg:flex justify-between items-center md:text-center mb-10 xl:mx-20 mx-10"
+    >
+      <div class="">
+        <img src="../assets/Shardic.png" class="mx-auto" alt="" />
+      </div>
+      <div class="lg:px-20">
+        <h4 class="font-bold tracking-wider text-2xl my-10">Shardic Bot</h4>
+        <p class="leading-8 text-gray-500 text-lg">
+          Blockchain-based bot for onchain token analysis. It uses the the
+          helius rpcs to gather information from the solana blockchain. It
+          scrapes the data and uses it to give the user insights about the token
+          and the market. Along token analysis, it also has a github repo
+          analyzer and a twitter username reuse checker. This project was built
+          using nextJS, tailwindCSS.
+        </p>
+        <div
+          class="w-fit my-10 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] mx-auto"
+        >
+          <a
+            href="https://shardicbot.com/"
+            class="uppercase py-3 px-14 text-lg text-white font-bold bg-emerald-500 rounded-md"
+            >visit</a
+          >
+        </div>
+      </div>
+    </div>
 
-            </div>
-
-            <div class="lg:flex justify-between items-center md:text-center mb-10 xl:mx-20 mx-10">
-                <div class="">
-                    <img src="../assets/moviespc.png" class="mx-auto" alt="">
-                </div>
-                <div class="lg:px-20">
-                    <h4 class="font-bold tracking-wider text-2xl my-10">Nuxt Movies</h4>
-                    <p class="leading-8 text-gray-500 text-lg">This was my first project with NuxtJS, this simplistic yet functional web application lets users explore and find movies they love using the MovieDB API. Through the development journey of this webapp, I've gained proficiency in constructing robust websites using NuxtJS and mastered implementing dynamic routing for a seamless user experience.</p>
-                    <div class="w-fit my-10 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] mx-auto">
-                        <a href="https://nuxt-movies-eta.vercel.app/" class="uppercase py-3 px-14 text-lg text-white font-bold bg-emerald-500 rounded-md">visit</a>
-                    </div>
-                </div>
-
-            </div>
-
-
-    </section>
+    <div
+      class="lg:flex justify-between items-center md:text-center mb-10 xl:mx-20 mx-10"
+    >
+      <div class="">
+        <img
+          src="../assets/chartwiz.png"
+          class="mx-auto w-[50rem] px-10"
+          alt=""
+        />
+      </div>
+      <div class="lg:px-20">
+        <h4 class="font-bold tracking-wider text-2xl my-10">Chartwiz</h4>
+        <p class="leading-8 text-gray-500 text-lg">
+          Chartwiz is a iOS app that allows the user to take a photo of a chart
+          and get insights about the chart. It uses the openAI API to analyze
+          the chart and give the user insights about the chart. This project was
+          built using React Native and OpenAI API.
+        </p>
+        <!-- <div
+          class="w-fit my-10 shadow-[rgba(0,_0,_0,_0.25)_0px_25px_50px_-12px] mx-auto"
+        >
+          <a
+            href="https://shardicbot.com/"
+            class="uppercase py-3 px-14 text-lg text-white font-bold bg-emerald-500 rounded-md"
+            >visit</a
+          >
+        </div> -->
+      </div>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
